6bf4ad824f096ecfb161d14d15cfff0e8077d950
Azaria Beryl <beryl.naga@gmail.com>
2025-11-17 09:04:58 +0700

    feat: add formatter

--- init.lua
+++ init.lua
@@ -413,7 +413,8 @@ require("lazy").setup({
           'lua_ls',
           -- 'vtsls',
           'ts_ls',
-          'cssmodules_ls'
+          'cssmodules_ls',
+          'eslint'
         },
         automatic_installation = false,
         handlers = function(server_name)
@@ -485,6 +486,47 @@ require("lazy").setup({
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
+    },
+  },
+
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
+        end
+      end,
+      formatters_by_ft = {
+        -- lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
     },
   }
 })
