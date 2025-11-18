a7ef90781b997cde0981ab9e90b85f5820291d19
Azaria Beryl <beryl.naga@gmail.com>
2025-11-18 13:10:36 +0700

    feat: use ctrl enter to accept recomendation

--- init.lua
+++ init.lua
@@ -602,7 +602,29 @@ require("lazy").setup({
 		version = "1.*",
 		opts = {
 			keymap = {
+				-- 'default' (recommended) for mappings similar to built-in completions
+				--   <c-y> to accept ([y]es) the completion.
+				--    This will auto-import if your LSP supports it.
+				--    This will expand snippets if the LSP sent a snippet.
+				-- 'super-tab' for tab to accept
+				-- 'enter' for enter to accept
+				-- 'none' for no mappings
+				--
+				-- For an understanding of why the 'default' preset is recommended,
+				-- you will need to read `:help ins-completion`
+				--
+				-- No, but seriously. Please read `:help ins-completion`, it is really good!
+				--
+				-- All presets have the following mappings:
+				-- <tab>/<s-tab>: move to right/left of your snippet expansion
+				-- <c-space>: Open menu or open docs if already open
+				-- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+				-- <c-e>: Hide menu
+				-- <c-k>: Toggle signature help
+				--
+				-- See :h blink-cmp-config-keymap for defining your own keymap
 				preset = "default",
+				["<C-CR>"] = { "accept" },
 			},
 
 			appearance = {
@@ -622,27 +644,6 @@ require("lazy").setup({
 				},
 			},
 
-			-- 'default' (recommended) for mappings similar to built-in completions
-			--   <c-y> to accept ([y]es) the completion.
-			--    This will auto-import if your LSP supports it.
-			--    This will expand snippets if the LSP sent a snippet.
-			-- 'super-tab' for tab to accept
-			-- 'enter' for enter to accept
-			-- 'none' for no mappings
-			--
-			-- For an understanding of why the 'default' preset is recommended,
-			-- you will need to read `:help ins-completion`
-			--
-			-- No, but seriously. Please read `:help ins-completion`, it is really good!
-			--
-			-- All presets have the following mappings:
-			-- <tab>/<s-tab>: move to right/left of your snippet expansion
-			-- <c-space>: Open menu or open docs if already open
-			-- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-			-- <c-e>: Hide menu
-			-- <c-k>: Toggle signature help
-			--
-			-- See :h blink-cmp-config-keymap for defining your own keymap
 			snippets = { preset = "default" },
 
 			-- Blink.cmp includes an optional, recommended rust fuzzy matcher,
