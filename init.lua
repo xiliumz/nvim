be9ebc4f489cb8ad320ed5d076d9d588e05694ed
Azaria Beryl <beryl.naga@gmail.com>
2026-03-02 11:32:32 +0700

    feat: add codediff

--- init.lua
+++ init.lua
@@ -368,30 +368,40 @@ require("lazy").setup({
 			end,
 		},
 	},
+
+	-- {
+	-- 	"sindrets/diffview.nvim",
+	-- 	opts = {},
+	-- 	keys = {
+	-- 		{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+	-- 		{
+	-- 			"<leader>gb",
+	-- 			function()
+	-- 				require("telescope.builtin").git_branches({
+	-- 					attach_mappings = function(_, map)
+	-- 						map("i", "<CR>", function(prompt_bufnr)
+	-- 							local selection = require("telescope.actions.state").get_selected_entry()
+	-- 							require("telescope.actions").close(prompt_bufnr)
+	-- 							vim.cmd("DiffviewOpen " .. selection.value)
+	-- 						end)
+	-- 						return true
+	-- 					end,
+	-- 				})
+	-- 			end,
+	-- 			desc = "Open Diffview Against a Branch",
+	-- 		},
+	-- 		{ "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+	-- 	},
+	-- },
+
 	{
-		"sindrets/diffview.nvim",
-		opts = {},
+		"esmuellert/codediff.nvim",
+		cmd = "CodeDiff",
 		keys = {
-			{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
-			{
-				"<leader>gb",
-				function()
-					require("telescope.builtin").git_branches({
-						attach_mappings = function(_, map)
-							map("i", "<CR>", function(prompt_bufnr)
-								local selection = require("telescope.actions.state").get_selected_entry()
-								require("telescope.actions").close(prompt_bufnr)
-								vim.cmd("DiffviewOpen " .. selection.value)
-							end)
-							return true
-						end,
-					})
-				end,
-				desc = "Open Diffview Against a Branch",
-			},
-			{ "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+			{ "<leader>gd", "<cmd>CodeDiff<cr>", desc = "Open Diffview" },
 		},
 	},
+
 	{
 		"kdheepak/lazygit.nvim",
 		lazy = true,
