1e99796dcd9b98a90c8d42b86b396ef99bd4aa46
Azaria Beryl <beryl.naga@gmail.com>
2026-07-07 10:55:07 +0700

    feat: enable format_on_save for all filetypes

--- init.lua
+++ init.lua
@@ -1017,20 +1017,10 @@ require("lazy").setup({
 		},
 		opts = {
 			-- notify_on_error = false,
-			format_on_save = function(bufnr)
-				-- Disable "format_on_save lsp_fallback" for languages that don't
-				-- have a well standardized coding style. You can add additional
-				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
-				if disable_filetypes[vim.bo[bufnr].filetype] then
-					return nil
-				else
-					return {
-						timeout_ms = 500,
-						lsp_format = "fallback",
-					}
-				end
-			end,
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
 			default_format_opts = {
 				stop_after_first = true,
 			},
