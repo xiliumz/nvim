9265df7761481469d1ce138916fbc87c554586f7
Azaria Beryl <88883166+xiliumz@users.noreply.github.com>
2026-08-03 16:08:07 +0700

    feat: add render-markdown for better markdown view

--- init.lua
+++ init.lua
@@ -866,6 +866,13 @@ require("lazy").setup({
 		opts = {},
 	},
 
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		opts = {},
+	},
+
 	{ -- Autocompletion
 		"saghen/blink.cmp",
 		event = "VimEnter",
@@ -1052,7 +1059,7 @@ require("lazy").setup({
 					"eslint_d",
 				},
 				typescriptreact = {
-					"prettierd",
+					-- "prettierd",
 					"eslint_d",
 				},
 				json = { "prettierd" },
