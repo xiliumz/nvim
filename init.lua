16a5079157ea6d9427f9adb03e31bea7d765f8fc
Azaria Beryl <88883166+xiliumz@users.noreply.github.com>
2026-09-14 15:49:12 +0700

    feat: add image renderer

--- init.lua
+++ init.lua
@@ -1064,4 +1064,17 @@ require("lazy").setup({
 			},
 		},
 	},
+
+	{
+		"3rd/image.nvim",
+		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+		opts = {
+			processor = "magick_cli",
+			integrations = {
+				markdown = {
+					enabled = false,
+				},
+			},
+		},
+	},
 })
